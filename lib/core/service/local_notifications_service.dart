e8d6bd16c0d2f5d0ee01b2f5306c8527e7b0aa3e
Seednov 'Alex' Alexander <seednov.alexander@gmail.com>
2025-01-28 15:41:43 +0300

    Basicly integrated

--- lib/core/service/local_notifications_service.dart
+++ lib/core/service/local_notifications_service.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_base/core/entity/push_entity.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 ///
 typedef HandleMessage = void Function(String? message);
@@ -9,30 +9,12 @@ typedef HandleMessage = void Function(String? message);
 /// Singleton
 ///
 /// Used only for Android
-// TODO(Alex): как я понял, текущий пакет не поддерживает отображение картинок..
-// Перейти на альтернативу - например
-// https://pub.dev/packages/awesome_notifications
 final class LocalNotificationsService {
-  ///
-  static const _androidIcon = '@drawable/notification';
-
-  ///
-  static const _androidInitializationSettings =
-      AndroidInitializationSettings(_androidIcon);
-
-  ///
-  static const _initializationSettings = InitializationSettings(
-    android: _androidInitializationSettings,
-  );
-
   /// Channel for getting local notifications on Android
-  late AndroidNotificationChannel _androidLocalChannel;
-
-  /// Settings for local notifications on Android
-  late AndroidNotificationDetails _androidLocalNotification;
+  late NotificationChannel _androidLocalChannel;
 
   /// Local notifications instance
-  final _instance = FlutterLocalNotificationsPlugin();
+  final _instance = AwesomeNotifications();
 
   /// **name** - Android application name for system notification settings
   Future<void> prepare({
@@ -40,46 +22,30 @@ final class LocalNotificationsService {
     required String name,
   }) async {
     /// Prepare settings
-    _androidLocalChannel = AndroidNotificationChannel(
-      // id
-      '$name-notifications',
-      // Name - в управлении уведомлениями в диспетчере приложений
-      '$name Push Notification',
-      description: 'Informing user',
-      importance: Importance.high,
-      enableLights: true,
+    _androidLocalChannel = NotificationChannel(
+      channelGroupKey: '$name-notifications-group',
+      channelKey: '$name-notifications',
+      channelName: '$name Push Notification',
+      channelDescription: 'Notification channel for informing user',
+      importance: NotificationImportance.Max,
     );
-
-    _androidLocalNotification = AndroidNotificationDetails(
-      _androidLocalChannel.id,
-      _androidLocalChannel.name,
-      channelDescription: _androidLocalChannel.description,
-      priority: Priority.high,
-      importance: Importance.high,
-      setAsGroupSummary: true,
-      styleInformation: const DefaultStyleInformation(true, true),
-      icon: _androidIcon,
-    );
-
-    /// And customize it
-    final androidPlugin = _instance.resolvePlatformSpecificImplementation<
-        AndroidFlutterLocalNotificationsPlugin>();
-
-    await androidPlugin?.createNotificationChannel(_androidLocalChannel);
 
     await _instance.initialize(
-      _initializationSettings,
-      onDidReceiveNotificationResponse: (NotificationResponse? data) =>
-          handleMessage(data?.payload),
+      // Use default application icon
+      null,
+      [_androidLocalChannel],
     );
   }
 
   /// Show local message
-  Future<void> show({required PushEntity pushEntity}) => _instance.show(
-        pushEntity.hashCode,
-        pushEntity.title,
-        pushEntity.body,
-        NotificationDetails(android: _androidLocalNotification),
-        payload: pushEntity.toString(),
+  Future<bool> show({required PushEntity pushEntity}) =>
+      _instance.createNotification(
+        content: NotificationContent(
+          id: pushEntity.hashCode,
+          channelKey: _androidLocalChannel.channelKey!,
+          title: pushEntity.title,
+          body: pushEntity.body,
+          payload: pushEntity.toMap(),
+        ),
       );
 }
